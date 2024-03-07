34925727d7143189d216fe4c1a1dca54dbc521f8
Yukino0113 <jethro0113@gmail.com>
2024-03-07 23:03:51 +0800

    Add new 🏠Home.py file with custom streamlit setup
    The new file, 🏠Home.py, includes the setup and initialization of the streamlit module, and it contains a function to set the page's title and icon. This stage serves as the foundation to present custom job descriptions and other Markdown content.

---
+++ pages/2_💻 _Portfolio.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from streamlit.logger import get_logger
+
+LOGGER = get_logger(__name__)
+
+
+def portfolio():
+    st.set_page_config(
+        page_title="Jethro_Porfolio",
+        page_icon="🦊",
+    )
+
+
+portfolio()
